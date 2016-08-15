32d4904c3614fe212e6745fa7af3d9f35851c006
Aljosha Friemann <aljosha.friemann@gmail.com>
2016-08-16 00:32:08 +0200

    basic logic done

--- cleaner/cli.py
+++ cleaner/cli.py
@@ -9,82 +9,47 @@
 
 """
 
-import os, tempfile, yaml
+import os, tempfile, yaml, datetime, logging
 import click, pykube
 
-def generate_config(host, token):
-    return {
-        "clusters": [
-            {
-                "name": "default",
-                "cluster": {
-                    "server": host,
-                }
-            }
-        ],
-        "contexts": [
-            {
-                "name": "default",
-                "context": {
-                    "cluster": "default",
-                    "namespace": "kube-system",
-                    "user": "default",
-                }
-            }
-        ],
-        "users": [
-            {
-                "name": "default",
-                "user": { "token": token } if token is not None else {},
-            }
-        ],
-        "current-context": "default",
-    }
-    # return """
-    # apiVersion: v1
-    # kind: Config
-    # clusters:
-    # - name: default
-    #   cluster:
-    #     api-version: v1
-    #     server: {host}
-    # users:
-    # - name: default
-    #   user:
-    #     token: eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJrdWJlLXN5c3RlbSIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJkZWZhdWx0LXRva2VuLXN2a3c0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6ImRlZmF1bHQiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC51aWQiOiIxZmRkNDJmNi01MzI2LTExZTYtODdmYi0wNjQ2NWUxOTY1ZmIiLCJzdWIiOiJzeXN0ZW06c2VydmljZWFjY291bnQ6a3ViZS1zeXN0ZW06ZGVmYXVsdCJ9.WU8Y7LHQ-lEmcfGPd1rhcOUhXSswB-ewcgBqPWEyu2FFPwtcGXq-v51sxI4Rzt--as6XbIoaFZWZ0CHC3XhO37taflFHOTvYP-kklJoiRqZPA1_BHoA6ruCD16esAYb5DOCUY7k70XTxx84-c1R1d_gB3yvHKFmNHrxiiqlhrIOfv5JXoPg_-D8A5gixMj1Q4dWybWf7eJblIOVA6hJdOt0Fv_Xjsr2y9H7NVSxEIPrc6bbqjVQZzDqi-pti1ttktKDOBBH1AwWxhPa0lfdkKmMiohHqopojaOt18ie_tpWWjhzRfOv3PL4mz88k3BSlwy8GNkO6O_RrkgjZ1b6J1Q
-    # contexts:
-    # - name: default
-    #   context:
-    #     cluster: default
-    #     namespace: kube-system
-    #     user: default
-    # current-context: default
-    # """.format(host=host)
+logger = logging.getLogger(__name__)
 
 def get_nodes(api):
     for node in pykube.Node.objects(api):
         yield node
+
+def delete_node(api, node_object):
+    pykube.Node(api, node_object).delete()
 
 @click.command()
 @click.option('--use-host-certs/--no-host-certs', default=True)
 @click.option('--insecure/--secure', default=False)
 @click.option('--token')
 @click.option('-s', '--api-server')
-def main(use_host_certs, insecure, token, api_server):
+@click.option('--time-shift', type=int, default=0, help='time shift between servers in seconds')
+def main(use_host_certs, insecure, token, api_server, time_shift):
     """main"""
     if api_server is not None:
-        config = pykube.KubeConfig(generate_config(api_server, token))
+        config = pykube.KubeConfig.from_url(api_server)
     else:
         config = pykube.KubeConfig.from_service_account()
 
     api = pykube.HTTPClient(config)
 
     for node in get_nodes(api):
-        print(10*'-' + str(node) + 10*'-')
-        print(dir(node))
-        print(node.annotations)
+        node_object = node.obj
+        for condition in node_object.get('status').get('conditions'):
+            if condition.get('type') != 'Ready': continue
+            if condition.get('status') == 'True': continue
 
+            last_heartbeat = datetime.datetime.strptime(condition.get('lastHeartbeatTime'), '%Y-%m-%dT%H:%M:%SZ')
+            shifted_heartbeat = last_heartbeat + datetime.timedelta(seconds=time_shift)
+            difference = (datetime.datetime.now() - shifted_heartbeat).seconds
 
+            if difference < 3600: continue
+
+            logger.info('deleting node %s', node_object.get('metadata').get('name'))
+            delete_node(api, node_object)
 
 if __name__ == '__main__':
     exit(main())
